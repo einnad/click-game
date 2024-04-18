2e100faced5448415ccfe920d588fc527688fa86
einnad <146109295+einnad@users.noreply.github.com>
2024-04-18 23:38:55 +0100

    Create grid and basic functions

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // elements
+const levelName = document.querySelector(".level-name");
+const clickItems = document.querySelectorAll(".click-box");
 
 // variables
+let level = 0;
 
 // functions
+function updateLevel() {
+  levelName.textContent = `Level ${level}`;
+}
+
+function checkSquare() {}
+
+function setTreasure() {
+  const randomNumber = Math.floor(Math.random() * 9) + 1;
+  console.log(randomNumber);
+  clickItems.forEach((i) => i.classList.add("hidden"));
+  clickItems[randomNumber].textContent = "T";
+}
+
+setTreasure();
 
 // event listeners
+
+// add hidden class to all
+// gen random number and change that square to T
+// when user clicks on T, remove hidden class on T
