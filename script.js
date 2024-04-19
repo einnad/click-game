d4afd5609358257f70e76239c13dac7693eabd6d
einnad <146109295+einnad@users.noreply.github.com>
2024-04-20 00:56:38 +0100

    Add level checks and click checks

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
 // elements
 const levelName = document.querySelector(".level-name");
+const levelCaption = document.querySelector(".level-caption");
 const clickItems = document.querySelectorAll(".click-box");
 const startButton = document.querySelector(".start");
 const resetButton = document.querySelector(".reset");
 
 // variables
 let level = 0;
+let click = 0;
 levelName.textContent = `Level ${level}`;
 
 // event listeners
 startButton.addEventListener("click", function () {
+  checkLevel();
   setTreasure();
   clickItems.forEach((i) => {
     i.addEventListener("click", () => {
       if (i.classList.contains("treasure")) {
         i.classList.remove("hidden");
         level++;
+        checkLevel();
         levelName.textContent = `Level ${level}`;
+        click = 0;
         setTimeout(() => {
           setTreasure();
         }, 2000);
+      } else {
+        click++;
+        if (click > 5) {
+          click = 0;
+          level = 0;
+          levelName.textContent = `Level ${level}`;
+        }
       }
     });
   });
 });
 
 resetButton.addEventListener("click", () => {
-  reset();
+  level = 0;
+  levelName.textContent = `Level ${level}`;
 });
 
 // functions
@@ -46,10 +59,24 @@ function setTreasure() {
   clickItems[randomNumber].classList.add("treasure");
 }
 
-function reset() {
-  setTreasure();
-}
-
 function generateNumber() {
   return Math.floor(Math.random() * 9);
+}
+
+function checkLevel() {
+  switch (level) {
+    case 3:
+      levelCaption.textContent = "Lucky 3 reached!";
+      break;
+    case 5:
+      levelCaption.textContent = "Lucky 5 reached!";
+      break;
+    case 7:
+      levelCaption.textContent = "Lucky 7 reached!";
+      break;
+    default:
+      levelCaption.textContent = "Keep clicking!";
+
+      break;
+  }
 }
