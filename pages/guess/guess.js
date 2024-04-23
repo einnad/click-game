2a31a6a2b01905c32edd52d156caaf5f9c366196
einnad <146109295+einnad@users.noreply.github.com>
2024-04-23 22:25:22 +0100

    Add guess game basics

--- pages/guess/guess.js
+++ pages/guess/guess.js
@@ -1,6 +1,38 @@
 // elements
 const chancesTag = document.querySelector(".chances");
+const userGuess = document.querySelector(".guess-input");
+const guessButton = document.querySelector(".guess-button");
+const resetButton = document.querySelector(".reset-button");
+
+// variables
+let guess;
+let clicks = 0;
+let computerNumber = Math.floor(Math.random() * 100) + 1;
 
 // event listeners
+guessButton.addEventListener("click", function () {
+  guess = +userGuess.value;
+  clicks++;
+  console.log(guess);
+  console.log(computerNumber);
+  switch (true) {
+    case guess === computerNumber:
+      chancesTag.textContent = "Correct!";
+      break;
+    case guess < computerNumber:
+      chancesTag.textContent = "Too low! Keep guessing!";
+      break;
+    case guess > computerNumber:
+      chancesTag.textContent = "Too high! Keep guessing!";
+      break;
+    default:
+      chancesTag.textContent = "Enter a valid number guess";
+  }
+});
+
+resetButton.addEventListener("click", () => resetGame());
 
 // functions
+function resetGame() {
+  computerNumber = Math.floor(Math.random() * 100) + 1;
+}
