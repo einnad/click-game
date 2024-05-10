1395c0c8dd838df10f7cb3b0e99ac944297ee21d
einnad <146109295+einnad@users.noreply.github.com>
2024-05-10 23:35:36 +0100

    Add checkhand func

--- pages/blackjack/blackjack.js
+++ pages/blackjack/blackjack.js
@@ -5,6 +5,8 @@ const dealButton = document.querySelector(".deal-button");
 const hitButton = document.querySelector(".hit-button");
 const standButton = document.querySelector(".stand-button");
 
+const comment = document.querySelector(".comment-div");
+
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]; // j, q, k all 10, ace 11 or 1
 playerCards = [];
 compCards = [];
@@ -13,12 +15,24 @@ function selectCard() {
   return cards[Math.floor(Math.random() * 12)];
 }
 
+function checkHand(hand) {
+  sum = 0;
+  hand.forEach((card) => {
+    sum += card;
+  });
+  return sum;
+}
+
 dealButton.addEventListener("click", function () {
   playerCards.push(selectCard());
   playerCards.push(selectCard());
 
   compCards.push(selectCard());
   compCards.push(selectCard());
+
+  while (checkHand(compCards) < 17) {
+    compCards.push(selectCard());
+  }
 
   console.log(playerCards);
   console.log(compCards);
@@ -29,6 +43,11 @@ dealButton.addEventListener("click", function () {
 
 hitButton.addEventListener("click", function () {
   playerCards.push(selectCard());
+  playerHand.textContent = playerCards;
+  if (checkHand(playerCards) > 21) {
+    comment.textContent = "Bust!";
+  }
+
   //   check card for ace
   //   check total for blackjack
   //   check for bust
