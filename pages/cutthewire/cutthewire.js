9c11923012bd123dc9fde6e9811e033a4910ff31
einnad <146109295+einnad@users.noreply.github.com>
2024-05-22 22:30:01 +0100

    Add cut wire visual and function

--- pages/cutthewire/cutthewire.js
+++ pages/cutthewire/cutthewire.js
@@ -1,9 +1,11 @@
 const wires = document.querySelectorAll(".wire");
 const heading = document.querySelector("h1");
+const twoWireDiv = document.querySelectorAll(".wire-double");
 
 const options = ["red", "blue", "green", "yellow", "pink"];
 
 // variable to control when game is complete to reset options
+// need to reset classlist
 // timer?
 let dangerOption;
 let safeOption;
@@ -20,6 +22,8 @@ window.addEventListener("load", function () {
 
 wires.forEach((wire) => {
   wire.addEventListener("click", function () {
+    let colour = wire.classList[1];
+    cut_wire(colour);
     if (wire.classList.contains(dangerOption)) {
       heading.textContent = "BANG! Wrong wire, game over.";
     } else if (wire.classList.contains(safeOption)) {
@@ -29,3 +33,12 @@ wires.forEach((wire) => {
     }
   });
 });
+
+function cut_wire(colour) {
+  twoWireDiv.forEach((div) => {
+    if (div.classList.contains(colour)) {
+      div.classList.add("cut-wire");
+      div.style.backgroundColor = "#000000";
+    }
+  });
+}
